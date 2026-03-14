98dd599f8961984c2e4877d8a229fbc7d97ab300
최선강 <Github tjsrkd02@naver.com>
2026-03-15 03:52:10 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -1,15 +1,15 @@
 a = 3
 b = 4
 
-#지수승 
+# 지수승 
 print(a ** b)
 print(a ** 3)
 
-#나머지 연산
+# 나머지 연산
 print(a % b)
 print(7 % 3)
 
-#나눗셈 몫 구하기
+# 나눗셈 몫 구하기
 print(a // b)
 print(7 // 3)
 
@@ -24,18 +24,18 @@ head = "Python"
 tail = " is fun"
 print(head + tail)
 
-#문자열 곱하기
+# 문자열 곱하기
 print(head * 2)
 print("=" * 5)
 
-#문자열 인덱싱
+# 문자열 인덱싱
 a = "Now is better than never"
 print(a[0])
 print(a[4])
 print(a[-1])
 print(a[-2])
 
-#문자열 슬라이싱
+# 문자열 슬라이싱
 b = a[0] + a[1] + a[2]
 print(b)
 
@@ -44,12 +44,12 @@ print(a[19:])
 print(a[:3])
 print(a[7:-11])
 
-#문자 개수 계산
+# 문자 개수 계산
 a = "Python"
 print(a.count("p"))
 
-#문자 위치 확인
+# 문자 위치 확인
 print(a.find('y'))
 print(a.find('p'))
 print(a.index('y'))
-#print(a.index('p'))
+# print(a.index('p'))
