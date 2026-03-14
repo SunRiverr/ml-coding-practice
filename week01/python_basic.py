1496d4f2fe4403406d5b70d913e323e5f3e96693
최선강 <Github tjsrkd02@naver.com>
2026-03-15 04:32:10 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -182,7 +182,7 @@ t2 * 10
 # 딕셔너리 만들기
 dic = {'name':'Hong', 'phone':'01012345678', 'birth':'0814'}
 
-#원소 추가
+# 원소 추가
 dic[1] = 'a'
 print(dic)
 
@@ -190,3 +190,8 @@ dic['pet'] = 'dog'
 print(dic)
 
 # 원소 삭제
+del dic[1]
+print(dic)
+
+# 원소의 value 구하기
+print(dic['phone'])
