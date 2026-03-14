6af701b5a09a03a8bd808f7074dddb3421e6bb61
최선강 <Github tjsrkd02@naver.com>
2026-03-15 05:29:41 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -423,9 +423,13 @@ f.close()
 # 파일 쓰기
 f = open('example.txt', 'w')
 for i in range(1, 6):
-    data = '%d번쨰 줄입니다. \n' % ㅑ
+    data = '%d번쨰 줄입니다. \n' % i
     f.write(data)
 f.close()
 
 # 파일에 내용 추가하기
-f = open
+f = open('example.txt', 'w')
+for i in range(1, 6):
+    data = '%d번쨰 줄입니다. \n' % i
+    f.write(data)
+f.close()
