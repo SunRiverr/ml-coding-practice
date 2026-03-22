578a6cbfc1e1c990998ece469dadcdf897a47cc7
최선강 <tjsrkd02@naver.com>
2026-03-22 23:11:30 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -153,4 +153,10 @@ import numpy as np
 
 print(np.array([1,2,3]))
 
-print(np.a)
+print(np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]]))
+
+print(np.array([[[1,2,3],
+                 [4,5,6],
+                 [7,8,9]]]))
