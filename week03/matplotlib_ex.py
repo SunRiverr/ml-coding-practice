55855e4511052a8393b8022f8bf2db84af652ead
최선강 <tjsrkd02@naver.com>
2026-03-29 20:11:16 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -227,6 +227,7 @@ plt.ylabel('Age')
 # x축의 눈금 설정
 plt.xticks([1, 2, 3], ['1st Class', '2nd Class', '3rd Class'])
 
+# 범례 설정
 plt.legend(violin_plot['bodies'], ['1st Class', '2nd Class', '3rd Class'],
            title='Pclass', loc="upper right")
 plt.savefig("Figure10.png")
