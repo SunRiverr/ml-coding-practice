d177bea07fb78309a97f6c3f9977543d54b51185
최선강 <tjsrkd02@naver.com>
2026-03-29 20:32:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -298,4 +298,16 @@ plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
 label='Not Survived')
 plt.xlabel('Parch')
 plt.ylabel('Not Survived Count', color='indigo')
-plt.tick_params(axis='y', label)
+plt.tick_params(axis='y', labelcolor='indigo')
+plt.legend(loc='upper right')
+
+# 두 번째 서브플롯 설정(막대 그래프)
+plt.subplot(2, 1, 2)
+plt.bar(x, y2, color='deeppink', alpha=0.7, width=0.5, label='Survived')
+plt.xlabel('Parch')
+plt.ylabel('Survived Count', color='deeppink')
+plt.tick_params(axis='y', labelcolor='deeppink')
+plt.legend(loc='upper right')
+
+# 제목 설정
+plt.suptitle('Survival ')
