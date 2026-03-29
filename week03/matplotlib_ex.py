a3b6e59166df82cced985dece60354fbacb40448
최선강 <tjsrkd02@naver.com>
2026-03-29 18:41:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -43,3 +43,6 @@ print(survived_counts)
 plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
 plt.title('Survived Counts by Embarked port on Titanic')
+plt.xlabel('Embarked Port')
+plt.ylabel('Count')
+plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', ])
