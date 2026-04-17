a715f7838d51108ff0c570da095649c5d4f32c36
최선강 <tjsrkd02@naver.com>
2026-04-17 21:19:39 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -51,23 +51,22 @@ def getNaverSearch(node, srcText, page_start, display):
         return json.loads(responsDecode)
     
 
-    def getRequestUrl(url):
-        req = urllib.request.Request(url)
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
 
-        req.add_header("X-Naver-Client-Id", client_id)
-        req.add_header("X-Naver-Client-Secret", client_secret)
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
 
-        try:
-            response = urllib.request.urlopen(req)
-            if response.getcode() == 200:
-                print("[%s] Url Request Success" % datetime.datetime.now())
-                return response.read().decode('utf-8')
-        except Exception as e:
-            print(e)
-            print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
-            return None
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
         
 
-        def getPostData(post, jsonResult, cnt):  #[CODE 3]
-            title = post['title']
-            
+def getPostData(post, jsonResult, cnt):  #[CODE 3]
+    title = post['title']
