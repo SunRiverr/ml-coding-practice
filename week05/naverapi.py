447242bec5c32cd21e0baaaa43922891a5bfdf27
최선강 <tjsrkd02@naver.com>
2026-04-17 21:18:09 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -60,4 +60,8 @@ def getNaverSearch(node, srcText, page_start, display):
         try:
             response = urllib.request.urlopen(req)
             if response.getcode() == 200:
-                print("[%s] Url ")
+                print("[%s] Url Request Success" % datetime.datetime.now())
+                return response.read().decode('utf-8')
+        except Exception as e:
+            print(e)
+            print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
