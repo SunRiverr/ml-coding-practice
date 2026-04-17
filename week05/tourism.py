e26d347741266117d4b9ddf0c219e4196e9239d4
최선강 <tjsrkd02@naver.com>
2026-04-17 20:45:08 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -57,21 +57,21 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
   
   """### [CODE 2]"""
 
-  def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
-    service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
-    parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
-    parameters += "&YM=" + yyyymm
-    parameters += "&NAT_CD=" + nat_cd
-    parameters += "&ED_CD=" + ed_cd
+def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
+  service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
+  parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
+  parameters += "&YM=" + yyyymm
+  parameters += "&NAT_CD=" + nat_cd
+  parameters += "&ED_CD=" + ed_cd
 
-    url = service_url + parameters
+  url = service_url + parameters
 
-    responseDecode = getRequestUrl(url)                       #[CODE 1]
+  responseDecode = getRequestUrl(url)                       #[CODE 1]
 
-    if (responseDecode == None):
-      return None
-    else:
-      return json.loads(responseDecode)
+  if (responseDecode == None):
+    return None
+  else:
+    return json.loads(responseDecode)
     
 """### [CODE 1]"""
 
