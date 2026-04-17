95e2cf8708b5cf30d813e2da1474a10e192a63fe
최선강 <tjsrkd02@naver.com>
2026-04-17 20:33:38 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -82,4 +82,6 @@ def getRequestUrl(url):  #[CODE 1]
     if response.getcode() == 200:
       print("[%s] Url Request Success" % datetime.datetime.now())
       return response.read().decode('utf-8')
-  except
+  except Exception as e:
+    print(e)
+    print("p[%s] Error for URL : %s" % (datetime.datetime.now))
