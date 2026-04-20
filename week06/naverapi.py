d9f8ddb13b6affe28442320dfa228876ba8f6a45
최선강 <tjsrkd02@naver.com>
2026-04-20 12:51:14 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -16,4 +16,7 @@ def main():
     jsonResponse = getNaverSearch(node, srcText, 1, 100)      #[CODE 2]
     total = jsonResponse['total']
 
-    while ((jsonResponse != None) and (jsonResponse['display']))
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']:
+            cnt += 1
+            getPostData(post, jsonResult, cnt)                  
