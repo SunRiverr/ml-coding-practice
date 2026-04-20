d5fbfd5ea310fbc14aeec514927b8090db2ec2da
최선강 <tjsrkd02@naver.com>
2026-04-20 13:21:45 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -33,8 +33,8 @@ def main():
         
         outfile.write(jsonFile)
 
-        print("가져온 데이터: %d 건" %(cnt))
-        print('%s_naver_%s_json SAVED' % (srcText, node))
+    print("가져온 데이터: %d 건" %(cnt))
+    print('%s_naver_%s_json SAVED' % (srcText, node))
     
 
 def getNaverSearch(node, srcText, page_start, display):
