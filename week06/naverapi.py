d02c6f5340817ba609f8af33d9812da6109a4e54
최선강 <tjsrkd02@naver.com>
2026-04-20 13:05:44 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -64,4 +64,4 @@ def getRequestUrl(url):
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
-         print("[%s] Url Request Success" % datetime.datetime.now())
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
