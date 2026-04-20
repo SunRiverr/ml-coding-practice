37be6e2cf664f32aee4a13c3922a043f14445248
최선강 <tjsrkd02@naver.com>
2026-04-20 12:49:14 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -8,4 +8,9 @@ client_secert = 'Client Sercret'
 
 def main():
 
-    node = 'news'
+    node = 'news'                                             #크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+    
+    cnt = 0
+    jsonResult = []
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)
