95c79db9ea2ac9e79bfd4bff5c17edf6a8c86118
최선강 <tjsrkd02@naver.com>
2026-05-05 21:38:07 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -134,4 +134,11 @@ plt.figure(figsize=(12, 6))
 
 sns.barplot(x=genres.values, y=genres.index, hue=genres.index, palette='RdGy')
 
-plt.title('')
+plt.title('Distribution of Genres for Movies and TV Shows on Netflix', fontsize=16)
+plt.xlabel('Count', fontsize=14)
+plt.ylabel('Genre', fontsize=14)
+plt.grid(axis='x')
+plt.show()
+
+netflix[netflix['title'].str.contains('Sankofa', na=False, case=False)]
+
