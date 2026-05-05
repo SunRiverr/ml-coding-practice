120769f484d10061e5019985c583ba08da2bb653
최선강 <tjsrkd02@naver.com>
2026-05-05 22:00:07 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -187,4 +187,24 @@ sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidth=2.5, annot=True,
 
 plt.suptitle('Target ages proportion of total content by country', 
              fontweight='bold', fontfamiy='serif', fontsize=15)
-plt.title('Here we see interesting differences between countries. Most shows in')
+plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
+          fontsize=12, fontfamily='serif')
+plt.show()
+
+# 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
+# wordCloud : 워드 클라우드 생성에 필요한 모듈
+# Image : 워드 클라우드를 원하는 형태로 그리기 위해 그림을 불러오는 패키지
+from wordcloud import WordCloud
+from PIL import Image
+
+plt.figure(figsize=(15, 5))
+
+# wordcloud에서 작동할 수 있도록 데이터 프레임을 list로 1차 변환시키고 str(문자열)로 2차 변환
+text = str(list(netflix['description']))
+
+# mask : 단어를 그릴 위치 설정, 흰색(#FFFFFF) 항목은 마스킹된 것으로 간주
+# 로고 이미지 열고 넘파이 배열로 변환
+mask = np.array(Image.open('netflix_logo.jpg'))
+
+# 워드 클라우드
