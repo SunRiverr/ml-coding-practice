d7e07ccac05f1b47c409c392c75b6100761de81e
최선강 <tjsrkd02@naver.com>
2026-05-05 19:57:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -18,13 +18,19 @@ def plot_digit(image_data):
     plt.imshow(image, camp='binary')
     plt.axis("off")
 
-    some_digit = X[0]
-    plot_digit(some_digit)
-    plt.show()
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show()
 
-    print(y[0])         # 샘플 데이터 레이블 확인 (=5)
+print(y[0])         # 샘플 데이터 레이블 확인 (=5)
 
-    # 10x10 그림 생성
-    plt.figure(figsize=(9, 9))
-    for idx, image_data in enumerate(X[:100]):
-        plt.
+# 10x10 그림 생성
+plt.figure(figsize=(9, 9))
+for idx, image_data in enumerate(X[:100]):
+    plt.subplot(10, 10, idx + 1)
+    plot_digit(image_data)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
