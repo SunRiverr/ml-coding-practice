72a18e89d918315f913be3aa59ba3dbda0e75fb5
최선강 <tjsrkd02@naver.com>
2026-05-05 21:12:37 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -59,4 +59,12 @@ age_group_dic = {
     'PG': 'Older Kids',
     'TV-Y7': 'Older Kids',
     'TV-Y7-FV': 'Older Kids',
+    'TV-PG': 'Older Kids',
+    'PG-13': 'Teens',
+    'TV-14': 'Young Adults',
+    'NC-17': 'Adults',
+    'NR': 'Adults',
+    'UR': 'Adults',
+    'R': 'Adults',
+    'TV-MA': 'Adults'
 }
